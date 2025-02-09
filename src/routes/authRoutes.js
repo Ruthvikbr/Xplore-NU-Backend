ee90ee039fa73cfaa8aae9f0eedc2f1c292670c9
Hitesh Soneta <hitesh@sute.app>
2025-02-08 19:34:37 -0500

    jwtauthentication

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { registerUser } = require("../controllers/authController");
+const { registerUser, loginUser } = require("../controllers/authController");
 const { getAllUsers } = require("../controllers/authController");
 
 // Define routes
 router.post("/register", registerUser);
+router.post("/login", loginUser);
 
 // Get all users route
 router.get("/users", getAllUsers);
-
 
 module.exports = router;
