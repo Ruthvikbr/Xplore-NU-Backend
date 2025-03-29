5cdb6d27bbe4e994eb63e8cd2ab7774fc20982f6
Nidhi Mehta <mehta.nidhi@northeastern.edu>
2025-03-29 14:38:11 -0400

    Created fetch poi description api

--- src/routes/buildingRoutes.js
+++ src/routes/buildingRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { createBuildings, getPOIs } = require('../controllers/buildingController');
+const { createBuildings, getPOIs, getBuildingById } = require('../controllers/buildingController');
 const { authenticateJWT } = require('../middleware/authMiddlewares');
 
 router.post('/', authenticateJWT, createBuildings); // Admin only
 router.get('/pois', authenticateJWT, getPOIs);
+router.get('/:id', authenticateJWT, getBuildingById);
 
 module.exports = router;
